8426dc58339ceaa23320b5ae655443a212f03d70
Decodon <20095352@mail.wit.ie>
2022-07-14 20:55:43 +0100

    Using react-query to manage the browser-based cache.

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,32 +1,33 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import PageTemplate from "../components/templateMovieListPage";
-import { getMovies } from "../api/tmdb-api";
+import { useQuery } from 'react-query'
+import Spinner from '../components/spinner'
+import {getMovies} from '../api/tmdb-api'
 
 const HomePage = (props) => {
-  const [movies, setMovies] = useState([]);
-  const favourites = movies.filter((m) => m.favourite);
-  localStorage.setItem("favourites", JSON.stringify(favourites));
+  const {  data, error, isLoading, isError }  = useQuery('discover', getMovies)
 
-  const addToFavourites = (movieId) => {
-    const updatedMovies = movies.map((m) =>
-      m.id === movieId ? { ...m, favourite: true } : m
-    );
-    setMovies(updatedMovies);
-  };
+  if (isLoading) {
+    return <Spinner />
+  }
 
-  useEffect(() => {
-    getMovies().then((movies) => {
-      setMovies(movies);
-    });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  if (isError) {
+    return <h1>{error.message}</h1>
+  }  
+  const movies = data.results;
+
+  // These three lines are redundant; we will replace them laterg.
+  const favourites = movies.filter(m => m.favouurite)
+  localStorage.setItem('favourites', JSON.stringify(favourites))
+  const addToFavourites = (movieId) => true 
 
   return (
     <PageTemplate
       title="Discover Movies"
       movies={movies}
       selectFavourite={addToFavourites}
-    />
+    />    
   );
 };
+
 export default HomePage;
